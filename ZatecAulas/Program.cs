5afde8af4e72c5a8b4bb1b1cb26aec4239ce33e1
jhonata Santos <jhow_aleixo@hotmail.com>
2023-11-07 23:29:00 -0300

    Aula0711 - Essa aula não teve nada, foi apenas introdutória.

---
+++ ZatecAulas/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZatecAulas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World, já fiz isso varias e varias vezes cara hsauhsuahsa");
+            Console.WriteLine(10 + 6);            
+            Console.ReadLine();
+        }
+    }
+}
